66c76720ee9df4f1069180ffd1bed2bc8def01a7
Yojen Grover <yojengrover@Yojens-MBP.fios-router.home>
2023-07-14 15:13:14 -0400

    Professional summary done

--- pages/index.js
+++ pages/index.js
@@ -28,33 +28,83 @@ export default function Home() {
       </nav>
       <div className=' text-center p-10'>
         <h2 className='text-5xl py-2 text-teal-600 font-medium'>Yojen Grover</h2>
-        <h3 className=' text-2xl py-2'>Frontend Developer</h3>
-        <p className='text-md py-5 leading-8 text-gray-800'>Frontend Developer/ Full Stack Developer experienced 
+        <h3 className=' text-2xl py-2 md:text-3xl'>JavaScript/Frontend Developer</h3>
+        <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>Frontend Developer/ Full Stack Developer experienced 
           in designing & developing custom websites based on 
           React/Redux and MERN Stack. </p>
       <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
         <AiFillLinkedin />
         <AiFillGithub />
       </div>
-      <div className=' relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'> 
+      <div className=' relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'> 
       <Image src={deved} layout="fill" objectFit='cover'/></div>
       </div>
     </section>
     <section>
       <div>
-        <h3 className='text-3xl'>Services I offer</h3>
+        <h3 className='text-3xl text-center'>Professional Summary</h3>
         <p className='text-md py-5 leading-8 text-gray-800'>
-        3+ years of experience in front-end development, 
-        Innovative and results-driven UI developer with over utilizing React, Redux,
-         and MobX for creating dynamic and responsive UI components.
+        <div className="bg-gray-100 p-8">
+  <h2 className="text-2xl font-bold mb-4">Front-end Development Experience</h2>
+
+  <ul className="list-disc ml-8">
+    <li className="mb-2">Innovative and results-driven UI developer with over 3+ years of experience in front-end development.</li>
+    <li className="mb-2">Proficient in utilizing React, Redux, and MobX for creating dynamic and responsive UI components.</li>
+    <li className="mb-2">Strong understanding of state management, routing, and debugging using React and Chrome development tools.</li>
+    <li className="mb-2">Skilled at analyzing and solving browser compatibility challenges.</li>
+    <li className="mb-2">Proficiency in building responsive and mobile-optimized websites using Bootstrap and Material UI.</li>
+  </ul>
+
+  <h2 className="text-2xl font-bold mt-8 mb-4">Back-end Development Experience</h2>
+
+  <ul className="list-disc ml-8">
+    <li className="mb-2">Proficient in back-end development with MongoDB and Node/Express.</li>
+    <li className="mb-2">Experienced in utilizing RESTful APIs to integrate data into web applications.</li>
+    <li className="mb-2">Skilled in Agile methodology and Scrum framework for efficient and collaborative software development.</li>
+  </ul>
+
+  <h2 className="text-2xl font-bold mt-8 mb-4">Other Skills</h2>
+
+  <ul className="list-disc ml-8">
+    <li className="mb-2">Adept at using JIRA for requirement gathering.</li>
+    <li className="mb-2">Experienced in Test Driven Development using Enzyme, React Testing Library, JEST, and achieving 100% coverage.</li>
+    <li className="mb-2">Proficient in using GIT and BITBUCKET for version control.</li>
+    <li className="mb-2">Possesses the ability to maintain consistency and well-commented HTML and CSS markup.</li>
+  </ul>
+</div>
+
         </p>
       </div>
-      <div>
-        <div>
+      <div className='lg:flex gap-10'>
+        <div className='text-center shadow-lg p-10 rounded-xl my-10'>
           <h4 className='text-lg font-medium pt-8 pb-2'>TechMates</h4>
           <p className='py-2'>Social media website for developers</p>
-          <p className='py-2'>Tech Stack: React.JS </p>
-
+          <p className='py-2 text-gray-800'><span >Tech Stack:</span> React.JS, MongoDB, Express, Node.js and SCSS</p>
+          <p className='py-2 text-gray-800'><span >GitHub: </span>
+          <a className='text-teal-600' href='https://github.com/yojengrover/Portfolio-Next.js'>
+            https://github.com/yojengrover/Portfolio-Next.js
+            </a> </p>
+          <p className='py-2'>Live:<span className='text-teal-700'> NA</span> </p>
+        </div>
+        <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+          <h4 className='text-lg font-medium pt-8 pb-2'>GasNGo</h4>
+          <p className='py-2'>Ride sharing application</p>
+          <p className='py-2 text-gray-800'><span >Tech Stack:</span> React.JS, MongoDB, GraphQl, Node.js, MaterilUI and Bootstrap</p>
+          <p className='py-2 text-gray-800'><span >GitHub: </span>
+          <a className='text-teal-600' href='https://github.com/yojengrover/Portfolio-Next.js'>
+            https://github.com/yojengrover/Portfolio-Next.js
+            </a> </p>
+          <p className='py-2'>Live:<span className='text-teal-700'> NA</span> </p>
+        </div>
+        <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+          <h4 className='text-lg font-medium pt-8 pb-2'>Portfolio</h4>
+          <p className='py-2'>Portfolio website using Next.js and tailwind</p>
+          <p className='py-2 text-gray-800'><span >Tech Stack:</span> Next.JS, JavaScript and tailwind</p>
+          <p className='py-2 text-gray-800'><span >GitHub: </span>
+          <a className='text-teal-600' href='https://github.com/yojengrover/Portfolio-Next.js'>
+            https://github.com/yojengrover/Portfolio-Next.js
+            </a> </p>
+          <p className='py-2'>Live:<span className='text-teal-700'> NA</span> </p>
         </div>
       </div>
     </section>
